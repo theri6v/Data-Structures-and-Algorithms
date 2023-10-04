613d6d6b47e416d26d3ab04367f46fce172a56d0
Rishav Walde <69449560+theri6v@users.noreply.github.com>
2023-10-05 01:42:47 +0530

    Create heap_sort1.py

---
+++ Sorting Algorithms/heap_sort1.py
@@ -0,0 +1,31 @@
+'''implementation of the Heap sort algorithm in Python'''
+
+
+def heapit(nums, heap_size, root_index):
+    largest = root_index
+    left_child = (2 * root_index) + 1
+    right_child = (2 * root_index) + 2
+    if left_child < heap_size and nums[left_child] > nums[largest]:
+        largest = left_child
+    if right_child < heap_size and nums[right_child] > nums[largest]:
+        largest = right_child
+    if largest != root_index:
+        nums[root_index], nums[largest] = nums[largest], nums[root_index]
+        heapit(nums, heap_size, largest)
+
+
+def heap_sort(nums):
+    n = len(nums)
+    for i in range(n, -1, -1):
+        heapit(nums, n, i)
+
+    for i in range(n - 1, 0, -1):
+        nums[i], nums[0] = nums[0], nums[i]
+        heapit(nums, i, 0)
+    return nums
+
+
+if __name__ == "__main__":
+    user_input = input("Enter numbers separated by a comma:\n").strip()
+    unsorted = [int(item) for item in user_input.split(",")]
+    print(heap_sort(unsorted))
