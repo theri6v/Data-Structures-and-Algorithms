77c58d9088f39b201f20fb3f338c8b069acab8b9
Rishav Walde <69449560+theri6v@users.noreply.github.com>
2023-10-04 22:28:12 +0530

    Create Counting_sort.py

---
+++ Sorting Algorithms/Counting_sort.py
@@ -0,0 +1,49 @@
+'''implementation of the Counting sort algorithm in Python'''
+
+
+def counting_sort(collection):
+    # if the collection is empty, returns empty
+    if collection == []:
+        return []
+
+    # get some information about the collection
+    coll_len = len(collection)
+    coll_max = max(collection)
+    coll_min = min(collection)
+
+    # create the counting array
+    counting_arr_length = coll_max + 1 - coll_min
+    counting_arr = [0] * counting_arr_length
+
+    # count how much a number appears in the collection
+    for number in collection:
+        counting_arr[number - coll_min] += 1
+
+    # sum each position with it's predecessors. now, counting_arr[i] tells
+    # us how many elements <= i has in the collection
+    for i in range(1, counting_arr_length):
+        counting_arr[i] = counting_arr[i] + counting_arr[i - 1]
+
+    # create the output collection
+    ordered = [0] * coll_len
+
+    # place the elements in the output, respecting the original order (stable
+    # sort) from end to begin, updating counting_arr
+    for i in reversed(range(0, coll_len)):
+        ordered[counting_arr[collection[i] - coll_min] - 1] = collection[i]
+        counting_arr[collection[i] - coll_min] -= 1
+
+    return ordered
+
+
+def counting_sort_string(string):
+    return "".join([chr(i) for i in counting_sort([ord(c) for c in string])])
+
+
+if __name__ == "__main__":
+    # Test string sort
+    assert "eghhiiinrsssttt" == counting_sort_string("thisisthestring")
+
+    user_input = input("Enter numbers separated by a comma:\n").strip()
+    unsorted = [int(item) for item in user_input.split(",")]
+    print(counting_sort(unsorted))
