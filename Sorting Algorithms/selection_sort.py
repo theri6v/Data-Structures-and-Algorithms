6d7561b3427b6888765ad49f836c093cd78a1ae1
Rishav Walde <69449560+theri6v@users.noreply.github.com>
2023-10-04 22:29:54 +0530

    Create selection_sort.py

---
+++ Sorting Algorithms/selection_sort.py
@@ -0,0 +1,19 @@
+'''implementation of the selection sort algorithm in Python'''
+
+
+def selection_sort(collection):
+    length = len(collection)
+    for i in range(length - 1):
+        least = i
+        for k in range(i + 1, length):
+            if collection[k] < collection[least]:
+                least = k
+        if least != i:
+            collection[least], collection[i] = (collection[i], collection[least])
+    return collection
+
+
+if __name__ == "__main__":
+    user_input = input("Enter numbers separated by a comma:\n").strip()
+    unsorted = [int(item) for item in user_input.split(",")]
+    print(selection_sort(unsorted))
