89e0f21af502517c358051aef2f2c4803165d138
Rishav Walde <69449560+theri6v@users.noreply.github.com>
2023-10-10 14:40:41 +0530

    Update implement factorial.py

--- Recursion/implement factorial.py
+++ Recursion/implement factorial.py
@@ -18,4 +18,18 @@ if __name__=='__main__':
 	n = 5;
 	print("factorial of",n,"is:",f(n))
 	
-	# This code is contributed by pratham76.
+	----------------------------------------------------------------------------------------------------------------------------------------------
+
+def factorial(n):
+	# Base case: if n is 0 or 1, return 1
+	if n == 0 or n == 1:
+		return 1
+
+	# Recursive case: if n is greater than 1, call the function with n-1 and multiply by n
+	else:
+		return n * factorial(n-1)
+
+# Call the factorial function and print the result
+result = factorial(5)
+print(result) # Output: 120
+
