0a6729015fca5ecd6cb5bd51de3c83ff7203602d
Rishav Walde <69449560+theri6v@users.noreply.github.com>
2023-10-11 22:08:26 +0530

    Update Sudoku Solver.py

--- Backtracking Algorithms/Sudoku Solver.py
+++ Backtracking Algorithms/Sudoku Solver.py
@@ -1,103 +1,133 @@
-# N is the size of the 2D matrix N*N
-N = 9
+'''A Backtracking program in Python to solve Sudoku problem'''
 
-# A utility function to print grid
-def printing(arr):
-	for i in range(N):
-		for j in range(N):
-			print(arr[i][j], end = " ")
-		print()
+# A Utility Function to print the Grid
+def print_grid(arr):
+	for i in range(9):
+		for j in range(9):
+			print (arr[i][j], end = " "),
+		print ()
 
-# Checks whether it will be
-# legal to assign num to the
-# given row, col
-def isSafe(grid, row, col, num):
-
-	# Check if we find the same num
-	# in the similar row , we
-	# return false
-	for x in range(9):
-		if grid[row][x] == num:
-			return False
-
-	# Check if we find the same num in
-	# the similar column , we
-	# return false
-	for x in range(9):
-		if grid[x][col] == num:
-			return False
-
-	# Check if we find the same num in
-	# the particular 3*3 matrix,
-	# we return false
-	startRow = row - row % 3
-	startCol = col - col % 3
-	for i in range(3):
-		for j in range(3):
-			if grid[i + startRow][j + startCol] == num:
-				return False
-	return True
-
-# Takes a partially filled-in grid and attempts
-# to assign values to all unassigned locations in
-# such a way to meet the requirements for
-# Sudoku solution (non-duplication across rows,
-# columns, and boxes) */
-def solveSudoku(grid, row, col):
-
-	# Check if we have reached the 8th
-	# row and 9th column (0
-	# indexed matrix) , we are
-	# returning true to avoid
-	# further backtracking
-	if (row == N - 1 and col == N):
-		return True
-	
-	# Check if column value becomes 9 ,
-	# we move to next row and
-	# column start from 0
-	if col == N:
-		row += 1
-		col = 0
-
-	# Check if the current position of
-	# the grid already contains
-	# value >0, we iterate for next column
-	if grid[row][col] > 0:
-		return solveSudoku(grid, row, col + 1)
-	for num in range(1, N + 1, 1):
-	
-		# Check if it is safe to place
-		# the num (1-9) in the
-		# given row ,col ->we
-		# move to next column
-		if isSafe(grid, row, col, num):
 		
-			# Assigning the num in
-			# the current (row,col)
-			# position of the grid
-			# and assuming our assigned
-			# num in the position
-			# is correct
-			grid[row][col] = num
-
-			# Checking for next possibility with next
-			# column
-			if solveSudoku(grid, row, col + 1):
+# Function to Find the entry in 
+# the Grid that is still not used
+# Searches the grid to find an 
+# entry that is still unassigned. If
+# found, the reference parameters 
+# row, col will be set the location
+# that is unassigned, and true is 
+# returned. If no unassigned entries
+# remains, false is returned.
+# 'l' is a list variable that has 
+# been passed from the solve_sudoku function
+# to keep track of incrementation 
+# of Rows and Columns
+def find_empty_location(arr, l):
+	for row in range(9):
+		for col in range(9):
+			if(arr[row][col]== 0):
+				l[0]= row
+				l[1]= col
 				return True
-
-		# Removing the assigned num ,
-		# since our assumption
-		# was wrong , and we go for
-		# next assumption with
-		# diff num value
-		grid[row][col] = 0
 	return False
 
-# Driver Code
+# Returns a boolean which indicates 
+# whether any assigned entry
+# in the specified row matches 
+# the given number.
+def used_in_row(arr, row, num):
+	for i in range(9):
+		if(arr[row][i] == num):
+			return True
+	return False
 
-# 0 means unassigned cells
-grid = [[3, 0, 6, 5, 0, 8, 4, 0, 0],
+# Returns a boolean which indicates 
+# whether any assigned entry
+# in the specified column matches 
+# the given number.
+def used_in_col(arr, col, num):
+	for i in range(9):
+		if(arr[i][col] == num):
+			return True
+	return False
+
+# Returns a boolean which indicates 
+# whether any assigned entry
+# within the specified 3x3 box 
+# matches the given number
+def used_in_box(arr, row, col, num):
+	for i in range(3):
+		for j in range(3):
+			if(arr[i + row][j + col] == num):
+				return True
+	return False
+
+# Checks whether it will be legal 
+# to assign num to the given row, col
+# Returns a boolean which indicates 
+# whether it will be legal to assign
+# num to the given row, col location.
+def check_location_is_safe(arr, row, col, num):
+	
+	# Check if 'num' is not already 
+	# placed in current row,
+	# current column and current 3x3 box
+	return (not used_in_row(arr, row, num) and
+		(not used_in_col(arr, col, num) and
+		(not used_in_box(arr, row - row % 3, 
+						col - col % 3, num))))
+
+# Takes a partially filled-in grid 
+# and attempts to assign values to
+# all unassigned locations in such a 
+# way to meet the requirements
+# for Sudoku solution (non-duplication 
+# across rows, columns, and boxes)
+def solve_sudoku(arr):
+	
+	# 'l' is a list variable that keeps the 
+	# record of row and col in 
+	# find_empty_location Function 
+	l =[0, 0]
+	
+	# If there is no unassigned 
+	# location, we are done 
+	if(not find_empty_location(arr, l)):
+		return True
+	
+	# Assigning list values to row and col 
+	# that we got from the above Function 
+	row = l[0]
+	col = l[1]
+	
+	# consider digits 1 to 9
+	for num in range(1, 10):
+		
+		# if looks promising
+		if(check_location_is_safe(arr, 
+						row, col, num)):
+			
+			# make tentative assignment
+			arr[row][col]= num
+
+			# return, if success, 
+			# ya ! 
+			if(solve_sudoku(arr)):
+				return True
+
+			# failure, unmake & try again
+			arr[row][col] = 0
+			
+	# this triggers backtracking	 
+	return False
+
+# Driver main function to test above functions
+if __name__=="__main__":
+	
+	# creating a 2D array for the grid
+	grid =[[0 for x in range(9)]for y in range(9)]
+	
+	# assigning values to the grid
+	grid =[[3, 0, 6, 5, 0, 8, 4, 0, 0],
 		[5, 2, 0, 0, 0, 0, 0, 0, 0],
 		[0, 8, 7, 0, 0, 0, 0, 3, 1],
 		[0, 0, 3, 0, 1, 0, 0, 8, 0],
@@ -106,10 +136,11 @@ grid = [[3, 0, 6, 5, 0, 8, 4, 0, 0],
 		[1, 3, 0, 0, 0, 0, 2, 5, 0],
 		[0, 0, 0, 0, 0, 0, 0, 7, 4],
 		[0, 0, 5, 2, 0, 6, 3, 0, 0]]
+	
+	# if success print the grid
+	if(solve_sudoku(grid)):
+		print_grid(grid)
+	else:
+		print ("No solution exists")
 
-if (solveSudoku(grid, 0, 0)):
-	printing(grid)
-else:
-	print("no solution exists ")
-
-	# This code is contributed by sudhanshgupta2019a
+# The above code has been contributed by Harshit Sidhwa.
