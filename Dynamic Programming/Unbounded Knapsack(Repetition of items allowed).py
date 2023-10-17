747b1e4ebacc6360daad6bbd5fb2acee403d0a18
Rishav Walde <69449560+theri6v@users.noreply.github.com>
2023-10-17 22:06:00 +0530

    Update Unbounded Knapsack(Repetition of items allowed).py

--- Dynamic Programming/Unbounded Knapsack(Repetition of items allowed).py
+++ Dynamic Programming/Unbounded Knapsack(Repetition of items allowed).py
@@ -29,4 +29,38 @@ n = len(val)
 
 print(unboundedKnapsack(W, n, val, wt)) 
 
+-------------------------------------------------------------------------------------
+#Memoization:  Like other typical Dynamic Programming(DP) problems, 
+#re-computation of same subproblems can be avoided by constructing a temporary array K[][] in bottom-up manner. 
+#Following is Dynamic Programming based implementation.
+
+def unboundedKnapsack(W, wt, val, idx, dp): 
+	# Base Case 
+	# if we are at idx 0. 
+	if idx == 0: 
+		return (W // wt[0]) * val[0] 
+	# If the value is already calculated then we will 
+	# previous calculated value 
+	if dp[idx][W] != -1: 
+		return dp[idx][W] 
+	# There are two cases either take element or not take. 
+	# If not take then 
+	notTake = 0 + unboundedKnapsack(W, wt, val, idx - 1, dp) 
+	# if take then weight = W-wt[idx] and index will remain 
+	# same. 
+	take = float('-inf') 
+	if wt[idx] <= W: 
+		take = val[idx] + unboundedKnapsack(W - wt[idx], wt, val, idx, dp) 
+	dp[idx][W] = max(take, notTake) 
+	return dp[idx][W] 
+
+# Driver code 
+W = 100
+val = [10, 30, 20] 
+wt = [5, 10, 15] 
+n = len(val) 
+dp = [[-1 for _ in range(W+1)] for _ in range(n)] 
+print(unboundedKnapsack(W, wt, val, n-1, dp)) 
+
+	
 
