2b99629c7ce4de9151d5edf5fcb1580bd8ba7aa0
Rishav Walde <69449560+theri6v@users.noreply.github.com>
2023-10-22 22:45:27 +0530

    Create find the mid-point of a line.py

---
+++ Geometric Algorithms/Lines/find the mid-point of a line.py
@@ -0,0 +1,15 @@
+
+'''Python3 program to find the midpoint of a line'''
+
+# Function to find the 
+# midpoint of a line 
+def midpoint(x1, x2, y1, y2): 
+
+	print((x1 + x2) // 2, " , ", 
+				(y1 + y2) // 2) 
+
+# Driver Code 
+x1, y1, x2, y2 = -1, 2, 3, -6
+midpoint(x1, x2, y1, y2) 
+
+
